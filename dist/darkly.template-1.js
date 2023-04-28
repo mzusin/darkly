e1955a4a796ea50792022cebfef1b75f9ad677b0
Miriam Zusin <miriamzusin@gmail.com>
2023-04-28 10:14:25 +0300

    package.json fix

--- dist/darkly.template-1.js
+++ dist/darkly.template-1.js
@@ -1,6 +1,6 @@
 /* 
 Darkly - Dark React Starter Template + UI Kit
-Version: 1.0.4
+Version: 1.0.2
 Author: Miriam Zusin     
 Contact: miriam.zusin@gmail.com
 Documentation: https://github.com/mzusin/darkly
